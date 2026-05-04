615a886c4748a2cfbb65844ad50a9249505411a0
Leeza Pithdiya <151375181+Leeza-2301@users.noreply.github.com>
2026-05-04 23:59:17 +0530

    update config files for css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx,mdx}",
+    "./pages/**/*.{js,jsx,ts,tsx,mdx}",
+    "./lib/**/*.{js,jsx,ts,tsx,mdx}"
+  ],
   theme: {
     extend: {
       colors: {
